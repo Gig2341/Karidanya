03d5b68080a5928301ec7ffa0e7021f3c94d41e3
Gig2341 <danmata2341@gmail.com>
2025-02-08 10:48:29 +0000

    controllers created

---
+++ controllers/productController.js
@@ -0,0 +1,68 @@
+const Product = require('../models/product');
+
+// Get all products
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Get a product by ID
+exports.getProductById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Create a new product
+exports.createProduct = async (req, res) => {
+    const { name, description, price, category, stock } = req.body;
+    const product = new Product({
+        name,
+        description,
+        price,
+        category,
+        stock,
+    });
+
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Update a product
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+
+        Object.assign(product, req.body);
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Delete a product
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+
+        await product.remove();
+        res.json({ message: 'Product removed' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
